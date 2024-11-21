d46211398b891407bd62b20d82314862ce5697c6
EKTA TRIVEDI <ekutrivedi2512@>
2024-11-21 11:43:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:animation_daily_task/view/greetting.dart';
+import 'package:animation_daily_task/view/plain.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GreetingAnimationPage(),
+    );
+  }
+}
